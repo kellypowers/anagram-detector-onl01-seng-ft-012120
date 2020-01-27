023a99e4703361170d1407254f9c034e6a83cccc
kellypowers <kpowers.nj@gmail.com>
2020-01-26 20:08:50 -0500

    passes tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,35 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+       
+
+    end
+
+    def match(array)
+        array_new = []
+        letters_in_initial_word_array = @word.split("")
+        #puts "let in intital word #{letters_in_initial_word_array}"
+        array.each do |words|
+            array_of_letters = words.split("")
+            #puts "array of letters #{array_of_letters}"
+            #letters_in_initial_word_array.each do |letter|
+                if array_of_letters.sort == letters_in_initial_word_array.sort
+                    #puts "array of letters #{array_of_letters} inital word letter array #{letters_in_initial_word_array}"
+                    array_new << words 
+                end
+            end
+            array_new
+
+                
+
+    end
+
+
+end
+
+diaper = Anagram.new('diaper')
+detector = Anagram.new('ba')
+ba = detector.match(['ab', 'abc', 'bac'])
